2cd5fa17c1e3e7a29f82b72e09c98400d0a91027
Vicente <vcl@uc.cl>
2022-05-20 21:07:52 -0400

    correcion

--- Sites/index.php
+++ Sites/index.php
@@ -12,7 +12,7 @@
 
   <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conexion.php");
+  require("config/conexion.php");
 
   #Se construye la consulta como un string
  	$query = "SELECT * FROM compania;";
