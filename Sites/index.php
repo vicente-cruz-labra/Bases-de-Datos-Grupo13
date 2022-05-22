919f745dc55cb541ccdcadb484ff90cb2684ef2c
Vicente <vcl@uc.cl>
2022-05-22 18:18:13 -0400

    arreglo consulta2

--- Sites/index.php
+++ Sites/index.php
@@ -22,7 +22,7 @@ tienen como destino el aeródromo.</h3>
 
     <?php
     #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require("../config/conexion.php");
+    require("config/conexion.php");
 
     #Se construye la consulta como un string
     $query = "SELECT nombre_compania FROM compania;";
