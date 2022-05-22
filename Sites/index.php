a953e8bb071538899ce57bba6737632ee3891aa3
Vicente <vcl@uc.cl>
2022-05-22 17:40:00 -0400

    consulta 1

--- Sites/index.php
+++ Sites/index.php
@@ -15,35 +15,41 @@
   require("config/conexion.php");
 
   #Se construye la consulta como un string
- 	$query = "SELECT * FROM compania;";
+ 	$query = "SELECT *
+   FROM vueloespecifico, vuelogenerico, compania, aeronave, costoticket 
+   WHERE vueloespecifico.codigo_vuelo = vuelogenerico.codigo_vuelo AND 
+   vueloespecifico.aeronave_id = aeronave.aeronave_id AND 
+   vueloespecifico.ruta_id = costoticket.ruta_id AND 
+   vuelogenerico.compania_id = compania.compania_id AND 
+   costoticket.aeronave_id = aeronave.aeronave_id AND 
+   vueloespecifico.estado = 'pendiente';";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
 	$result -> execute();
-	$companias_total = $result -> fetchAll();
+	$resultado_consulta1 = $result -> fetchAll();
   ?>
 
-<b>FUNCIONAL</b>
-
+<table>
 <table>
     <tr>
-      <th>ID</th>
-      <th>Nombre</th>
-      <th>Código</th>
+      <th>VUELO ID</th>
+      <th>RUTA ID</th>
+      <th>CÓDIGO VUELO</th>
+      <th>AERONAVE ID</th>
+      <th>FECHA SALIDA</th>
+      <th>FECHA LLEGADA</th>
+      <th>VELOCIDAD</th>
+      <th>ALTITUD</th>
+      <th>ESTADO</th>
     </tr>
 </table> 
-
-<table>
-    <tr>
-      <th>ID</th>
-      <th>Nombre</th>
-      <th>Código</th>
-    </tr>
   
       <?php
         // echo $companias_total;
-        foreach ($companias_total as $p) {
-          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td></tr>";
+        foreach ($resultado_consulta1 as $p) {
+          echo "<tr><td>$p[0]</td><td>$p[1]</td><td>$p[2]</td><td>$p[3]</td><td>$p[4]</td>
+          <td>$p[5]</td><td>$p[6]</td><td>$p[7]</td><td>$p[8]</td></tr>";
       }
       ?>
       
