b5130fb4959bd6843e7d655f1dfd940783f5d466
Vicente <vcl@uc.cl>
2022-05-22 18:49:02 -0400

    consulta 2 listo

--- Sites/consultas/consulta2.php
+++ Sites/consultas/consulta2.php
@@ -15,7 +15,8 @@
   WHERE vueloespecifico.codigo_vuelo = vuelogenerico.codigo_vuelo AND 
   vuelogenerico.compania_id = compania.compania_id AND 
   vueloespecifico.estado = 'aceptado' AND
-  compania.nombre_compania = '$a';";
+  compania.nombre_compania = '$a'
+  vuelogenerico.aerodromo_llegada_id = '$c';";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
