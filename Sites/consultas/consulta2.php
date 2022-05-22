ecb24e6983a201b8ce84cdcb42f972b096a083ab
Vicente <vcl@uc.cl>
2022-05-22 18:35:50 -0400

    arreglar consulta 2

--- Sites/consultas/consulta2.php
+++ Sites/consultas/consulta2.php
@@ -15,9 +15,9 @@
   WHERE vueloespecifico.codigo_vuelo = vuelogenerico.codigo_vuelo AND 
   vuelogenerico.compania_id = compania.compania_id AND 
   vueloespecifico.estado = 'aceptado' AND
-  compania.nombre_compania = 'IBERIA' AND
+  compania.nombre_compania = $a AND
   vuelogenerico.aerodromo_llegada_id = aerodromo.aerodromo_id AND
-  aerodromo.aerodromo_id = 'EEGZ'
+  aerodromo.codigo_icao = $c
   );";
 
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
