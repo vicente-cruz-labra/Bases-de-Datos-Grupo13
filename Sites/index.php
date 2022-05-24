4168875483d56cf20d881b1cb43ffa6c66d9f711
Vicente <vcl@uc.cl>
2022-05-23 21:05:56 -0400

    actualizar estilo

--- Sites/index.php
+++ Sites/index.php
@@ -2,12 +2,12 @@
 
 <body>
 
-  <h1 align="center">Biblioteca Vuelos </h1>
+  <h1 align="center">Consultas Vuelos </h1>
   
   <br>
 
   <p>
-  <h3 align="center"> 1. Muestre todos los vuelos pendientes de ser aprobados por la DGAC</h3>
+  <h3 align="center"> Vuelos pendientes de ser aprobados por la DGAC</h3>
 
   <form action="consultas/consulta1.php" method="get">
     <input type="submit" value="Buscar">
@@ -18,9 +18,8 @@
   <br>
   <br>
 
-  <h3 align="center"> 2. Dado un código ICAO de un aeródromo ingresado por el usuario y una aerolínea
-seleccionada por el usuario, liste todos los vuelos aceptados de dicha aerolínea que
-tienen como destino el aeródromo.</h3>
+  <h3 align="center"> 2. Vuelos aceptados por cierta aerolínea que
+tienen como destino el aeródromo del código</h3>
 
     <?php
     #Llama a conexión, crea el objeto PDO y obtiene la variable $db
@@ -53,7 +52,7 @@ tienen como destino el aeródromo.</h3>
   <br>
   <br>
 
-  <h3 align="center"> 3. Dado un código de reserva ingresado por el usuario, liste los tickets asociados a esta
+  <h3 align="center"> 3. Tickets asociados al código, 
 junto a sus pasajeros y costos</h3>
 
   <form align="center" action="consultas/consulta3.php" method="post">
@@ -67,7 +66,7 @@ junto a sus pasajeros y costos</h3>
   <br>
   <br>
 
-  <h3 align="center"> 4. Por cada aerolinea, muestre al cliente que ha comprado la mayor cantidad de tickets</h3>
+  <h3 align="center"> 4. Por cada aerolinea, el cliente que ha comprado la mayor cantidad de tickets</h3>
 
   <form action="consultas/consulta4.php" method="get">
     <input type="submit" value="Buscar">
@@ -77,29 +76,12 @@ junto a sus pasajeros y costos</h3>
   <br>
   <br>
 
-  <h3 align="center"> 5. Al ingresar el nombre de una aerolínea, liste la cantidad de vuelos que tienen en cada
-uno de los estados</h3>
-
-<?php
-    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require("config/conexion.php");
-
-    #Se construye la consulta como un string
-    $query = "SELECT nombre_compania FROM compania;";
-
-    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-    $result = $db -> prepare($query);
-    $result -> execute();
-    $nombres_companias = $result -> fetchAll();
-    ?>
+  <h3 align="center"> 5. Cantidad de vuelos que tienen en cada
+uno de los estados la aerolínea ingresada</h3>
 
   <form align="center" action="consultas/consulta5.php" method="post">
     Aerolínea:
-    <select name="nombre_compania">
-      <?php
-        foreach ($nombres_companias as $p) {echo "<option value=$p[0]>$p[0]</option>";}
-      ?>
-    </select>
+    <input type="text" name="nombre_compania">
     <br/><br/>
     <input type="submit" value="Buscar">
   </form>
@@ -108,7 +90,7 @@ uno de los estados</h3>
   <br>
   <br>
 
-  <h3 align="center"> 6. Muestre la aerolínea que tiene el mayor porcentaje de vuelos aceptados</h3>
+  <h3 align="center"> 6. Aerolínea que tiene el mayor porcentaje de vuelos aceptados</h3>
 
   <form action="consultas/consulta6.php" method="get">
     <input type="submit" value="Buscar">
